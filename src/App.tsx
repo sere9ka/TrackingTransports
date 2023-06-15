891db4cffcf962671d7d86c9bf7efef37f86cab4
sere9ka <s.kovylov@zhiet.ru>
2023-06-15 13:34:25 +0300

    feat: add component Main & Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Header } from './components/Header/header';
+import { Main } from './components/Main/main';
+import { AppContext, Transport } from './context/AppContext';
 
-function App() {
+
+
+export const App = () => {
+
+  const [transports, setTransports] = useState([])
+
+  const res = fetch('./list.json')
+                .then(res => res.json())
+                .then(data => console.log(data))
+                .catch(error => console.error(error))  
+
+  const app = {
+    pages: ["Список ТС", "Экран ТС", "Настройки"],
+    category: ["Грузовой", "Пассажирский", "Спецтранспорт"],
+    transports: []
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <AppContext.Provider value={app}>
+        <Main />
+      </AppContext.Provider>
+    </>
   );
 }
-
-export default App;
